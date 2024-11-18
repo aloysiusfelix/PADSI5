d8488546dfd8c4d4fbc2fe7e8af9999a4a9d3d79
aloysiusfelix <aloysiusfelixsantoso@gmail.com>
2024-11-18 23:03:04 +0700

    pembelian bisa dikit

--- app/Http/Controllers/PembelianController.php
+++ app/Http/Controllers/PembelianController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Pembelian;
 use App\Models\Stok;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PembelianController extends Controller
 {
@@ -32,7 +33,6 @@ class PembelianController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'id_pembelian' => 'required|unique:transaksi_pembelian',
             'tanggal_pembelian' => 'required|date',
             'id_stok' => 'required|exists:stok,id_stok',
             'jumlah_item_pembelian' => 'required|integer|min:1',
@@ -41,12 +41,15 @@ class PembelianController extends Controller
         // Retrieve selected stock item
         $stok = Stok::findOrFail($request->id_stok);
 
+        // Generate unique ID pembelian
+        $id_pembelian = 'PB' . strtoupper(Str::random(6));
+
         // Calculate total price
         $total_harga_pembelian = $stok->harga_stok * $request->jumlah_item_pembelian;
 
         // Create the purchase record
         Pembelian::create([
-            'id_pembelian' => $request->id_pembelian,
+            'id_pembelian' => $id_pembelian,
             'tanggal_pembelian' => $request->tanggal_pembelian,
             'id_stok' => $stok->id_stok,
             'nama_stok' => $stok->nama_stok,
--- resources/views/pembelian/create.blade.php
+++ resources/views/pembelian/create.blade.php
@@ -5,10 +5,7 @@
     <h1>Tambah Pembelian</h1>
     <form action="{{ route('pembelian.store') }}" method="POST">
         @csrf
-        <div class="mb-3">
-            <label for="id_pembelian" class="form-label">ID Pembelian</label>
-            <input type="text" id="id_pembelian" name="id_pembelian" class="form-control" required>
-        </div>
+     
         <div class="mb-3">
             <label for="tanggal_pembelian" class="form-label">Tanggal Pembelian</label>
             <input type="date" id="tanggal_pembelian" name="tanggal_pembelian" class="form-control" required>
@@ -26,7 +23,7 @@
         </div>
         <div class="mb-3">
             <label for="jumlah_item_pembelian" class="form-label">Jumlah Item Pembelian</label>
-            <input type="number" id="jumlah_item_pembelian" name="jumlah_item_pembelian" class="form-control" required>
+            <input type="number" id="jumlah_item_pembelian" name="jumlah_item_pembelian" class="form-control" min="1" required>
         </div>
         <button type="submit" class="btn btn-primary">Tambah Pembelian</button>
     </form>
