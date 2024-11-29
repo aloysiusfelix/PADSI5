c5383280aab6678f0f0e2ffe9cc21c8e2cacd0cf
aloysiusfelix <aloysiusfelixsantoso@gmail.com>
2024-11-29 23:01:16 +0700

    gAS

--- resources/views/errors/unauthorized.blade.php
+++ resources/views/errors/unauthorized.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container text-center" style="margin-top: 10%; color: #555;">
-    <h1 style="font-size: 80px; font-weight: bold; color: #d9534f;">Visca Barca, Visca Cataluna</h1>
+    <h1 style="font-size: 80px; font-weight: bold; color: #d9534f;">!! ERROR !!</h1>
     <h3>Unauthorized</h3>
     <p>Anda tidak memiliki izin untuk mengakses halaman ini. Silakan hubungi administrator jika diperlukan.</p>
     <a href="{{ route('penjualan.index') }}" class="btn btn-primary btn-lg mt-3">Kembali ke Penjualan</a>
